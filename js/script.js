74773265d396f8fb79093e3f84393a136d8d8713
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-03-24 16:12:53 +0100

    feat: :sparkles: Adds: almost completition of both exercises

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// ESERCIZIO BICICLETTE
+//stabilisco un Array di biciclette, specificandone nome e peso
 const bikesArray = [
     {
         nome: "Bianchi",
@@ -13,10 +15,57 @@ const bikesArray = [
         peso: 5,
     },
 ];
-
+// debug
 console.log(bikesArray);
-
-let pesoMinimoArray = Math.min(...bikesArray.peso);
+// calcolo quale, dall'Array, è la bicicletta col valore "peso" più basso
+// stabilisco prima una variabile che comprende solo il valore del peso
+let pesoBikesArray = bikesArray.peso;
+let pesoMinimoArray = Math.min(pesoBikesArray);
 console.log(pesoMinimoArray);
+// e la stampo
+document.getElementsByClassName("exercise1-results").innerHTML = `${pesoMinimoArray}`;
 
-const
+// ESERCIZIO SQUADRE DI CALCIO
+// stabilisco un Array di squadre di calcio specificandone
+// nome
+// punti fatti
+// falli subiti
+// ma attribuirò 0 a questi ultimi due fattori
+const footballSquads = [
+    {
+        nome: "Lazio",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },    {
+        nome: "Roma",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },    {
+        nome: "Sampdoria",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },    {
+        nome: "Napoli",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },    {
+        nome: "Salernitana",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    }, 
+];
+ // Genero numero dei punti fatti tra 1 e 12
+ /* const punti = Math.floor(Math.random() * 12) + 1;
+
+ const falli = Math.floor(Math.random() * 12) + 1; */
+
+ for (let i = 0; i < footballSquads.length; i++) {
+    footballSquads[i].puntiFatti = Math.floor(Math.random() * 12) + 1;
+    footballSquads[i].falliSubiti = Math.floor(Math.random() * 12) + 1;
+ }
+
+ const footballSquadsStat = footballSquads.map (({nome, falliSubiti}) => ({nome, falliSubiti}));
+ console.log(footballSquadsStat);
+
+ 
+ document.getElementsByClassName("exercise2-results").innerHTML = `${footballSquadsStat}`;
